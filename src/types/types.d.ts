560560c9b50f80bd2ab01f5c942a0cb834ccabbf
mj <mjmartinsu@yahoo.es>
2025-01-08 13:26:58 +0100

    Advance resolution GoT

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,10 +1,12 @@
+export type Category = "king" | "fighter" | "adviser" | "squire";
+
 export type Char = {
   isAlive: boolean;
   message: string;
   name: string;
   family: string;
   age: number;
-  category: string;
+  category: Category;
   reignYears?: number;
   weapon?: string;
   skillLevel?: number;
